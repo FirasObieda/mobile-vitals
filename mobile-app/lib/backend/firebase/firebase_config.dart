a4ddf9ff70d34c09139ed20e6d4bc6164c7ac0d8
Firas <140647296+FirasObieda@users.noreply.github.com>
2026-09-09 15:47:54 +0400

    removed api keys

--- mobile-app/lib/backend/firebase/firebase_config.dart
+++ mobile-app/lib/backend/firebase/firebase_config.dart
@@ -5,7 +5,7 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-            apiKey: "AIzaSyDfA24C8cL9yE3D-fOaWGmcOF2jLyaffuY",
+            apiKey: "",
             authDomain: "sdp-08-health-tracking-app.firebaseapp.com",
             projectId: "sdp-08-health-tracking-app",
             storageBucket: "sdp-08-health-tracking-app.firebasestorage.app",
